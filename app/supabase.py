f695ad202d3ffd1ebc0f9ef5b2edf5162d0d2915
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2024-11-24 19:55:22 +0100

    establecer conexión con supabse

--- app/supabase.py
+++ app/supabase.py
@@ -1,25 +1,19 @@
-# Importamos las bibliotecas necesarias
-from supabase import create_client  # Para interactuar con Supabase.
-# Para cargar las variables de entorno desde un archivo .env.
-from dotenv import load_dotenv
-# Para acceder a las variables de entorno del sistema operativo.
 import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-# Cargar las variables del archivo .env
-# La ruta relativa se especifica como "../.env", lo que indica que el archivo .env se encuentra en el directorio superior al actual.
-load_dotenv(dotenv_path="../.env")
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
-# Obtener las variables de entorno necesarias para la conexión con Supabase.
-# Estas variables deben estar definidas en el archivo .env para no ser hardcodeadas.
-SUPABASE_URL = os.getenv("SUPABASE_URL")  # La URL de Supabase.
-# La clave de API (por ejemplo, clave pública o servicio).
+# Obtener la URL de Supabase desde las variables de entorno
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+
+# Obtener la clave de Supabase desde las variables de entorno
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-# Crear un cliente para interactuar con Supabase usando las variables de entorno obtenidas.
-# La función `create_client` necesita tanto la URL como la clave para poder conectarse correctamente.
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-# Verificar si las variables de entorno no están definidas.
-# Si alguna de las variables esenciales está ausente, se lanzará un error.
+# Verificar si las variables de entorno están definidas
 if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("Faltan variables de entorno SUPABASE_URL o SUPABASE_KEY")
+    raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
+
+# Crear el cliente de Supabase utilizando la URL y la clave
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)

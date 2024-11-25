8f6e90847879c6fe74656a19806a88d5d931aef1
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2024-11-25 15:33:36 +0100

    se crea la clase que llama a la api

--- app/supabase_data.py
+++ app/supabase_data.py
@@ -2,32 +2,39 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
+class SupabaseAPI:
 
-# Obtener la URL de Supabase desde las variables de entorno
-SUPABASE_URL = os.getenv("SUPABASE_URL")
+    def __init__(self, tabla, select):
+        # Cargar las variables de entorno desde el archivo .env
+        load_dotenv()
 
-# Obtener la clave de Supabase desde las variables de entorno
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+        # Obtener la URL de Supabase desde las variables de entorno
+        SUPABASE_URL = os.getenv("SUPABASE_URL")
+        
+        # Obtener la clave de Supabase desde las variables de entorno
+        SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-# Verificar si las variables de entorno están definidas
-if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
+        # Verificar si las variables de entorno están definidas
+        if not SUPABASE_URL or not SUPABASE_KEY:
+            raise ValueError("SUPABASE_URL and SUPABASE_KEY must be set")
 
-# Crear el cliente de Supabase utilizando la URL y la clave
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+        # Crear el cliente de Supabase utilizando la URL y la clave
+        self.supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# Ejecutar la consulta y obtener los resultados
-response = supabase.table("users").select("*").execute()
+        self.tabla = tabla
+        self.select = select
 
-# Depuración adicional
-print("Datos de la respuesta:", response.data)
+    def fetch_data(self):
+        return self.supabase.table(self.tabla).select(self.select).execute()
 
-# Verificar y mostrar los resultados
-if response.data:
-    print("Datos obtenidos de la tabla 'test':")
-    for record in response.data:
-        print(record)
-else:
-    print("No se encontraron registros en la tabla 'users'.")
+# Ejemplo de uso
+if __name__ == "__main__":
+    api = SupabaseAPI("users", "surname")
+    response = api.fetch_data()
+    print("Datos de la respuesta:", response.data)
+    if response.data:
+        print("Datos obtenidos de la tabla 'users':")
+        for record in response.data:
+            print(record)
+    else:
+        print("No se encontraron registros en la tabla 'users'.")

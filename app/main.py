23078b97f099e7749849d90d9ab156c6933b6b44
VforVitorio <victorvegasobral@yahoo.com>
2024-12-01 13:52:10 +0100

    eliminado archivo user_routes

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@
 from fastapi import FastAPI
 from app.supabase_data import SupabaseAPI
 # Importamos el router que contiene las rutas relacionadas con los usuarios.
-# Importamos las rutas definidas en user_routes.py.
-from app.routes.user_routes import user_router
+
+
 from typing import List, Optional
 from pydantic import BaseModel
 
@@ -14,10 +14,6 @@ import json
 # FastAPI es un marco de trabajo para construir APIs rápidas con Python 3.7+.
 app = FastAPI()
 
-# Incluir el router de 'user_router' que contiene las rutas relacionadas con los usuarios
-# `prefix="/api"`: Define un prefijo común para todas las rutas dentro del `user_router`, de manera que todas las rutas definidas en `user_router` tendrán la URL prefijada con `/api`.
-# `tags=["Usuarios"]`: Etiqueta las rutas con "Usuarios" para mejorar la organización y visualización en la documentación generada automáticamente por FastAPI (Swagger).
-app.include_router(user_router, prefix="/api", tags=["Usuarios"])
 
 # Ruta adicional para la raíz de la aplicación ("/")
 

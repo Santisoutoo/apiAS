2d49b8c7569cfd804a9450f8582832b5a055afab
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2024-12-11 21:39:48 +0100

    agregar función que verifica si tiene permiso admin

--- app/routes/oauth.py
+++ app/routes/oauth.py
@@ -39,7 +39,15 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         username: str = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=401, detail="Token inválido")
-        return username
+        return payload  # Devolver el payload completo si contiene más datos (como el rol)
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido o expirado")
 
+# Dependencia para verificar el rol de administrador
+def verify_admin_role(current_user: dict = Depends(get_current_user)):
+    if current_user.get("role") == "admin":
+        return current_user
+    else:
+        raise HTTPException(
+            status_code=403, detail="Acceso denegado. Necesitas permisos de administrador."
+        )
